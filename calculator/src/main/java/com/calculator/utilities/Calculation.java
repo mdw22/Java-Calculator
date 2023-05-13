7f18ea19707479b28e34e5674b5831fb7d8cd48a
mdw22 <mdw22@email.sc.edu>
2023-05-13 04:35:19 -0400

    V 0.1 Commit. Added Java Application launch and inital buttons.

--- calculator/src/main/java/com/calculator/utilities/Calculation.java
+++ calculator/src/main/java/com/calculator/utilities/Calculation.java
@@ -1,4 +1,4 @@
-package com.calculator; 
+package com.calculator.utilities;
 public class Calculation {
     
 }
--- calculator/src/main/java/com/calculator/utilities/CalculatorFrame.java
+++ calculator/src/main/java/com/calculator/utilities/CalculatorFrame.java
@@ -3,9 +3,13 @@
  */
 package com.calculator.utilities;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 
 public class CalculatorFrame extends JFrame {
     public CalculatorFrame() {
@@ -16,14 +20,60 @@ public class CalculatorFrame extends JFrame {
         JPanel panel = new JPanel();
         
         // Add a button to the panel
-        JButton button = new JButton("Click me!");
-        panel.add(button);
+        // JButton button = new JButton("Click me!");
+        // panel.add(button);
+        JLabel test = new JLabel("Test");
+        test.setFont(new Font(Font.SANS_SERIF, 0, 36));
         
         // Add the panel to the center of the window
-        add(panel, BorderLayout.CENTER);
+        add(test, BorderLayout.PAGE_START);
+
+        // Create the button panel
+        panel.setLayout(new GridLayout(4, 4));
+
+         // Create the number buttons
+         JButton button0 = new JButton("0");
+         JButton button1 = new JButton("1");
+         JButton button2 = new JButton("2");
+         JButton button3 = new JButton("3");
+         JButton button4 = new JButton("4");
+         JButton button5 = new JButton("5");
+         JButton button6 = new JButton("6");
+         JButton button7 = new JButton("7");
+         JButton button8 = new JButton("8");
+         JButton button9 = new JButton("9");
+ 
+         // Create the operation buttons
+         JButton buttonPlus = new JButton("+");
+         JButton buttonMinus = new JButton("-");
+         JButton buttonMultiply = new JButton("*");
+         JButton buttonDivide = new JButton("/");
+         JButton buttonEquals = new JButton("=");
+         JButton buttonClear = new JButton("C");
+ 
+         // Add the buttons to the panel
+         panel.add(button7);
+         panel.add(button8);
+         panel.add(button9);
+         panel.add(buttonDivide);
+         panel.add(button4);
+         panel.add(button5);
+         panel.add(button6);
+         panel.add(buttonMultiply);
+         panel.add(button1);
+         panel.add(button2);
+         panel.add(button3);
+         panel.add(buttonMinus);
+         panel.add(button0);
+         panel.add(buttonEquals);
+         panel.add(buttonClear);
+         panel.add(buttonPlus);
+ 
+         // Add the button panel to the calculator panel
+         add(panel, BorderLayout.CENTER);
         
         // Set the size of the window
-        setSize(500, 750);
+        setSize(500, 800);
         
         // Set the location of the window
         setLocationRelativeTo(null);
@@ -33,5 +83,9 @@ public class CalculatorFrame extends JFrame {
         
         // Exit the application when the window is closed
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        CalculatorFrame frame = new CalculatorFrame();
     }
 }
